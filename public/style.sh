6e917e31ad1615d278f7f5390429918b81a55b6b
tiko2015 <tiko2015@gmail.com>
2013-10-16 12:13:25 -0300

    creación de estilos en linea y ejemplo

--- public/style.sh
+++ public/style.sh
@@ -1,2 +1,4 @@
 lessc -x less/style.less > style.css
 lessc -x less/style_black.less > style_black.css
+lessc -x less/style_inline.less > style_inline.min.css
+lessc less/style_inline.less > style_inline.css
